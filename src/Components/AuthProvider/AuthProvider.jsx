2b90415ac174c0406499b2d9c7ae0f62f12262d3
NTNirjana <nafijatabassum79@gmail.com>
2025-10-28 20:09:38 +0600

    reload issue fix

--- src/Components/AuthProvider/AuthProvider.jsx
+++ src/Components/AuthProvider/AuthProvider.jsx
@@ -1,3 +1,51 @@
+// /* eslint-disable react-refresh/only-export-components */
+// import { createContext,useEffect,useState } from "react";
+
+// export const AuthContext = createContext();
+
+// const AuthProvider = ({ children }) => {
+//  const [userData, setUserData] = useState(null);
+//   const [loading, setLoading] = useState(false);
+
+//   console.log("333", userData?.name)
+
+//     useEffect(() => {
+//     const storedUser = localStorage.getItem("userInfo");
+//     if (storedUser) {
+//       setUserData(JSON.parse(storedUser));
+//     }
+//   }, []);
+
+
+//   const handleLogout = () => {
+//     localStorage.removeItem("accessToken");
+//     localStorage.removeItem("userInfo");
+//     setUserData(null);
+//   };
+
+
+//   useEffect(() => {
+//     if (userData) {
+//       localStorage.setItem("userInfo", JSON.stringify(userData));
+//     }
+//   }, [userData]);
+
+//   // Context Data
+//   const authInfo = {
+//     userData,
+//     loading,
+//     setLoading,
+//     handleLogout,
+//     setUserData,
+    
+//   };
+
+//   return (
+//     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+//   );
+// };
+
+// export default AuthProvider;
 
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useEffect, useState } from "react";
